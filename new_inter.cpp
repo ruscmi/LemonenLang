71e6b87addd46ba48dce3d84c4e5476391b9e983
ruscmi <korom250@gmail.com>
2026-06-21 20:16:14 +0300

    начинаю делать парсер

--- new_inter.cpp
+++ new_inter.cpp
@@ -1,29 +1,121 @@
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <vector>
 #include <unordered_map>
-#include <cctype>
+#include <vector>
+bool ActiveRequest = true;
 using namespace std;
 #pragma region
-enum TTYPES { KEYWORD, OPERATOR, SEPARATOR, NUMBER, STRING, UNKNOWN };
-struct Token { TTYPES KEY; string VAL; };
-class LEX{
-public:
-    vector<Token>tokenize(const string& code) {
-    	vector<Token> tokens;
-    	unsigned int i=0;
-    	unsigned int len = code.length();
-        string val = "";
-    	while( i < len ) { 
-    	    unsigned char current = code[i];
-    	    if(isspace(current)) { i++; continue; } 
-    	    if(isdigit(current)) { Token T; while(i < len && isdigit(code[i])){ current = current + code[i]; i++;  } tokens.push_back({TTYPES::NUMBER,val }); continue; }
-    	}
-    	return tokens;
-    }    
+enum TTYPE { KEYWORD, OPERATOR, SEPARATOR, NUMBER, STRING, UNKNOWN };
+struct Token {
+  TTYPE KEY;
+  string VAL;
 };
-int main() { 
-        string code; LEX Lexer cout<<"󰣇 󰁔"; getline(cin,code) 
-        
+struct Node {
+  TTYPE KEY;
+  string VAL;
+  int left_index;
+  int right_index;
+};
+class LEX {
+private:
+  vector<Token> tokens;
+  Token T;
+public:
+  vector<Token>& tokenize(const string &code) {
+    tokens.clear();
+    unsigned int i = 0;
+    unsigned int len = code.length();
+    while (i < len) {
+      unsigned char current = code[i];
+      if (isspace(current)) {
+        i++;
+        continue;
+      }
+      if (isdigit(current)) {
+        string val = "";
+        while (i < len && isdigit(code[i])) {
+          val += code[i];
+          i++;
+        }
+        T.KEY = TTYPE::NUMBER;
+        T.VAL = val;
+        tokens.push_back(T);
+        continue;
+      }
+      if (isalpha(current)) {
+        string val = "";
+        while (i < len && isalpha(code[i])) {
+          val += code[i];
+          i++;
+        }
+        T.KEY = TTYPE::STRING;
+        T.VAL = val;
+        tokens.push_back(T);
+        continue;
+      }
+      if (current == '+' || current == '-' || current == '*' ||
+          current == '/' || current == '=' || current == '<' ||
+          current == '>') {
+        string val(1, current);
+        T.KEY = TTYPE::OPERATOR;
+        T.VAL = val;
+        tokens.push_back(T);
+        i++;
+        continue;
+      }
+      if (current == '.' || current == '{' || current == '}' ||
+          current == '(' || current == ')') {
+        string val(2, current);
+        T.KEY = TTYPE::SEPARATOR;
+        T.VAL = val;
+        tokens.push_back(T);
+        i++;
+        continue;
+      }
     }
+    return tokens;
+  }
+  void vector_parsing() {
+    for (const auto &token : tokens) {
+      cout << token.KEY << " : " << token.VAL << " ";
+    }
+    cout << endl;
+  }
+};
+class parser {
+private:
+    unsigned int position = 0;
+    vector<Token> tokenize;
+public:
+	parser(vector<Token> tokenize){
+		this->tokenize=tokenize;
+	}
+	Token peer() {
+		if(position >= tokenize.size()) {
+			return Token{TTYPE::UNKNOWN,""};
+		}
+		return tokenize[position];
+	}
+	Token advanced() {
+		if(position < tokenize.size()) {
+			position++; 
+		}
+		return tokenize[position -1];
+	}
+};
+int main() {
+  LEX lexing;
+  string code;
+  while (ActiveRequest) {
+    cout << "#> ";
+    getline(cin, code);
+    lexing.tokenize(code);
+    lexing.vector_parsing();
+    if (code == "exit") {
+      ActiveRequest = false;
+      break;
+    }
+  }
+}
 #pragma endregion
